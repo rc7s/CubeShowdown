10c11fc8b2a5df982658691ebd9a6fa2af6ed866
Ryan Chin <ryanchin@me.com>
2016-11-27 12:14:54 -0500

    instantiate on round over

--- master_epic_cubes/Assets/scripts/Kill.cs
+++ master_epic_cubes/Assets/scripts/Kill.cs
@@ -5,15 +5,25 @@ public class Kill : MonoBehaviour {
 
 	public AudioSource audio;
 	public AudioClip clip;
+	public int deaths;
+
+	public GameObject spriteFab1;
+	public GameObject spriteFab2;
+	public GameObject spriteFab3;
 
 	// Use this for initialization
 	void Start () {
-
+		deaths = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (deaths == 3) {
+			Instantiate (spriteFab1, new Vector3(-185f,22.5f,-21f), transform.rotation);
+			Instantiate (spriteFab2, new Vector3(-101f,22.5f,-3f), transform.rotation);
+			Instantiate (spriteFab3, new Vector3(-169f,22.5f,73f), transform.rotation);
+			deaths = 0;
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -21,6 +31,7 @@ public class Kill : MonoBehaviour {
 		{
 			Destroy(other.gameObject);
 			audio.Play ();
+			deaths += 1;
 		}
 	}
 }

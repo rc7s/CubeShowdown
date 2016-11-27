b7d1523bae3da068412a0e6ac26d39d1448e225d
Ryan Chin <ryanchin@me.com>
2016-11-27 01:58:45 -0500

    no message

---
+++ master_epic_cubes/Assets/PlayerMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour {
+
+	public int playerId;
+	private Player player;
+	private CharacterController cc;
+	public Rigidbody rb;
+	private float lStickX;
+	private float lStickY;
+	private float rStickX;
+	private float rStickY;
+
+	// Use this for initialization
+	void Awake () {
+		 // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+        player = ReInput.players.GetPlayer(playerId);
+
+        // Get the character controller
+        cc = GetComponent<CharacterController>();
+
+		rb = GetComponent<Rigidbody> ();
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetInput();
+		ProcessInput();
+	}
+
+	private void GetInput(){
+		lStickX = player.GetAxis ("StickLeftX");
+		lStickY = player.GetAxis ("StickLeftY");
+		rStickX = player.GetAxis ("StickRightX");
+		rStickY = player.GetAxis ("StickRightY");
+	}
+
+	private void ProcessInput(){
+		
+			rb.AddForce (0, lStickY * 20, 0, ForceMode.Impulse);
+			rb.AddForce (lStickX * 20, 0, 0, ForceMode.Impulse);
+
+	}
+}

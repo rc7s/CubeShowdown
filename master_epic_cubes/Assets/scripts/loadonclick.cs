7ebb08deb1d61df3701a408b6760fec01b1d4b14
Daniel Jiang <djj286@nyu.edu>
2016-11-29 13:26:13 -0500

    Mateo's Start Screen

---
+++ master_epic_cubes/Assets/scripts/loadonclick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadonclick : MonoBehaviour
+{
+    public GameObject loadingImage;
+
+
+    public void LoadScene(int level)
+    {
+
+        loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+
+
+    }
+
+}
